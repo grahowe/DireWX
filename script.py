0c43b6ae68297c646ea50394bf76bcf5095c9ced
Owen Graham <omgraham02@gmail.com>
2025-02-05 14:04:42 -0600

    Changed API retrieval based on lat/long

--- script.py
+++ script.py
@@ -1,16 +1,20 @@
+# Code written by Owen Graham, KE0SBX
+# This script can be injected directly into the direwolf.conf file
+
 # #!/usr/bin/env python3
-# Uncomment the line above if on Linux
+# Uncomment the shebang line above if on Direwolf Linux
 
 # Requests library for API access
 import requests
 
-# Change this to reflect your UGC code
-# You can find this on the National Weather Service's website or Google
-zone = ""
+# Change this to reflect your decimal latitude and longitude
+# Default is set to my QTH
+lat = "40.3461"
+long = "-94.8725"
 
 # Function to fetch weather alerts from the NWS API
 def fetch_nws_alerts():
-    url = "https://api.weather.gov/alerts/active/zone/" + zone
+    url = "https://api.weather.gov/alerts/active?point=" + lat + "," + long
 
     # Try-catch/except for API access
     # If it succeeds, the request will return the information
@@ -47,7 +51,7 @@ def main():
         for alert_type in alert_types:
             print(f"WX Alert: {alert_type}. Take precautions now!")
     else:
-        print("No active weather alerts found.")
+        print("No active weather alerts.")
 
 if __name__ == "__main__":
     main()
